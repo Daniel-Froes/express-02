ace8c272493105e2458c4a7deacf5708d22aba48
artiscruelty <danielfroestbf@gmail.com>
2022-04-06 20:40:11 -0300

    feat: adicionando a rota

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const path = require('path')
 const app = express();
 const port = 3333
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname,'views/index.html')))
-app.get('/', (req, res) => res.sendFile(path.join(__dirname,'views/sobre.html')))
+app.get('/index', (req, res) => res.sendFile(path.join(__dirname,'views/index.html')))
+app.get('/sobre', (req, res) => res.sendFile(path.join(__dirname,'views/sobre.html')))
 
 
 app.listen(port, () => console.info(`Servidor rodando na porta ${port}`))
